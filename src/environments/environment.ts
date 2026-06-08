6d2d8129542c2f5f2d344bf60436b2cd0347cbf9
Garv <bsc@gmail>
2026-06-08 21:15:36 +0530

    Connect frontend to Render backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://auraecommercee.netlify.app/api'
+  production: true,
+  apiUrl: 'https://auraecommerce-assessment.onrender.com/api'
 };
